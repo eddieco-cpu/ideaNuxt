56905fbded92cf33b8d12d0b1a5b2066f14b5e6b
Eddie-Ko <eddie01241@gmail.com>
2024-02-07 17:27:08 +0800

    更新 | rights

---
+++ components/rights/RightsProgress.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="bg-blue-200 w-3/4 m-auto">
+        <div class="bg-blue-600" :style="{ width: `${percent}%` }">Bar</div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    total: Number,
+    price: Number,
+});
+
+const percent = computed(() => {
+    const result = props.price / props.total;
+    return result > 1 ? 100 : result * 100;
+});
+</script>
