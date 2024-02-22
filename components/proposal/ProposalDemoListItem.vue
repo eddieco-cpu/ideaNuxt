c4d53971cd06cedc7c6529b0774fa7f989374a4d
udnlinyi <130973011+udnlinyi@users.noreply.github.com>
2024-02-22 17:30:57 +0800

    更新 | 提案介紹頁

--- components/proposal/ProposalDemoListItem.vue
+++ components/proposal/ProposalDemoListItem.vue
@@ -1,12 +1,17 @@
 <!-- 子組件 -->
 <template>
-    <li class="flex justify-start items-start max-w-[640px]" :class="class">
-        <b class="font-roboto text-8xl mr-3 text-Primary-200">
+    <li
+        class="flex justify-start items-start max-w-[640px] max-md:flex-col max-md:items-center max-md:px-5"
+        :class="class"
+    >
+        <b class="font-roboto text-8xl mr-3 text-Primary-200 max-md:text-[40px] max-md:mr-0 max-md:mb-2">
             {{ dataObj.index }}
         </b>
-        <article class="text-left">
-            <h3 class="text-2xl font-bold text-Primary-500-Primary mb-2">{{ dataObj.title }}</h3>
-            <p class="text-base leading-7 font-normal" v-html="dataObj.content"></p>
+        <article class="text-left max-md:text-center">
+            <h3 class="text-2xl font-bold text-Primary-500-Primary mb-2 max-md:text-xl max-md:mb-3">
+                {{ dataObj.title }}
+            </h3>
+            <p class="text-base leading-7 font-normal max-md:text-sm" v-html="dataObj.content"></p>
         </article>
     </li>
 </template>
--- components/proposal/ProposalIntroCard.vue
+++ components/proposal/ProposalIntroCard.vue
@@ -1,18 +1,24 @@
 import type { Html } from '#build/components';
 <!-- 子組件 -->
 <template>
-    <section class="max-w-[590px] bg-white rounded-3xl border-2 border-Primary-100 p-10 shadow-card" :class="class">
-        <div class="flex justify-between items-center mb-5">
+    <section
+        class="max-w-[590px] bg-white rounded-3xl border-2 border-Primary-100 p-10 shadow-card max-md:p-6"
+        :class="class"
+    >
+        <div class="flex justify-between items-center mb-5 max-md:items-end max-md:mb-0">
             <div class="text-left">
-                <h3 class="font-medium text-xl text-Primary-500-Primary">{{ dataObj.note }}</h3>
-                <p class="text-4xl leading-tight font-bold text-Primary-500-Primary">
+                <h3 class="font-medium text-xl text-Primary-500-Primary max-md:text-xl">{{ dataObj.note }}</h3>
+                <p class="text-4xl leading-tight font-bold text-Primary-500-Primary max-md:hidden">
                     {{ dataObj.title }}
                 </p>
             </div>
-            <img :src="'/_nuxt/assets/images/proposal/' + dataObj.photo" class="block w-[100px]" />
+            <img :src="'/_nuxt/assets/images/proposal/' + dataObj.photo" class="block h-[94px] max-md:h-[73px]" />
         </div>
+        <p class="text-2xl leading-tight font-bold text-Primary-500-Primary md:hidden max-md:mb-4">
+            {{ dataObj.title }}
+        </p>
         <article>
-            <p class="text-base leading-7 font-normal" v-html="dataObj.content"></p>
+            <p class="text-base leading-7 font-normal max-md:text-sm" v-html="dataObj.content"></p>
         </article>
     </section>
 </template>
@@ -25,3 +31,38 @@ const props = defineProps({
     },
 });
 </script>
+<style scoped>
+.gradient-to-t {
+    background-color: transparent;
+    background: linear-gradient(to top, #fff, rgba(255, 255, 255, 0) 30%);
+    min-height: 350px;
+}
+.gradient-to-t > * {
+    opacity: 0;
+}
+.gradient-to-b {
+    background-color: transparent;
+    background: linear-gradient(to bottom, #fff, rgba(255, 255, 255, 0) 30%);
+    min-height: 350px;
+}
+.gradient-to-b > * {
+    opacity: 0;
+}
+
+@media not all and (min-width: 1280px) {
+    .gradient-to-t {
+        background: linear-gradient(to top, #fff, #fff);
+        min-height: auto;
+    }
+    .gradient-to-t > * {
+        opacity: 1;
+    }
+    .gradient-to-b {
+        background: linear-gradient(to bottom, #fff, #fff);
+        min-height: auto;
+    }
+    .gradient-to-b > * {
+        opacity: 1;
+    }
+}
+</style>
