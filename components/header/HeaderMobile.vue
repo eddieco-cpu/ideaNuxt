66627d8b4557cf48f1e7ac38326fa29cbcf4d326
Ivan.chen <Ivan.chen@advantech.com.tw>
2024-02-22 13:07:58 +0800

    更新 | header

--- components/header/HeaderMobile.vue
+++ components/header/HeaderMobile.vue
@@ -45,126 +45,126 @@
                 </div>
             </div>
         </div>
-    </header>
 
-    <!-- 側邊選單 -->
-    <div>
-        <Transition name="mask">
+        <!-- 側邊選單 -->
+        <div>
+            <Transition name="mask">
+                <div
+                    class="fixed left-0 top-0 h-full w-full bg-black bg-opacity-50 transition duration-1000 z-50"
+                    @click="hideSideNav = true"
+                    v-if="!hideSideNav"
+                ></div>
+            </Transition>
+
             <div
-                class="fixed left-0 top-0 h-full w-full bg-black bg-opacity-50 transition duration-1000 z-50"
-                @click="hideSideNav = true"
-                v-if="!hideSideNav"
-            ></div>
-        </Transition>
-
-        <div
-            class="w-64 fixed left-0 top-0 h-full bg-white overflow-x-auto transition duration-500 transform z-50"
-            :class="{ '-translate-x-full': hideSideNav }"
-        >
-            <div class="auth py-4 px-7 flex items-center bg-Primary-500-Primary">
-                <!-- 會員未登入 -->
-                <button
-                    class="rounded-lg bg-Primary-500-Primary border border-Primary-200 text-white px-3 py-1.5 text-sm flex gap-x-1 items-center justify-center"
-                    v-if="!store.isLogin"
-                    @click="openModal('login')"
-                >
-                    <img src="~assets/images/header/user.svg" />
-
-                    <span class="font-normal"> 登入 /註冊 </span>
-                </button>
-
-                <!-- 會員已登入 -->
-                <div class="flex" v-else>
+                class="w-64 fixed left-0 top-0 h-full bg-white overflow-x-auto transition duration-500 transform z-50"
+                :class="{ '-translate-x-full': hideSideNav }"
+            >
+                <div class="auth py-4 px-7 flex items-center bg-Primary-500-Primary">
+                    <!-- 會員未登入 -->
                     <button
                         class="rounded-lg bg-Primary-500-Primary border border-Primary-200 text-white px-3 py-1.5 text-sm flex gap-x-1 items-center justify-center"
-                        @click="goToPage('/member/information')"
+                        v-if="!store.isLogin"
+                        @click="openModal('login')"
                     >
-                        <img
-                            :src="store.userInfo.image"
-                            alt="memberPic"
-                            class="block rounded-full w-[18px] h-[18px] object-cover"
-                        />
+                        <img src="~assets/images/header/user.svg" />
 
-                        <span class="font-normal"> 會員中心 </span>
+                        <span class="font-normal"> 登入 /註冊 </span>
                     </button>
 
-                    <button class="underline text-white text-sm ml-3" @click="logout">登出</button>
+                    <!-- 會員已登入 -->
+                    <div class="flex" v-else>
+                        <button
+                            class="rounded-lg bg-Primary-500-Primary border border-Primary-200 text-white px-3 py-1.5 text-sm flex gap-x-1 items-center justify-center"
+                            @click="goToPage('/member/information')"
+                        >
+                            <img
+                                :src="store.userInfo.image"
+                                alt="memberPic"
+                                class="block rounded-full w-[18px] h-[18px] object-cover"
+                            />
+
+                            <span class="font-normal"> 會員中心 </span>
+                        </button>
+
+                        <button class="underline text-white text-sm ml-3" @click="logout">登出</button>
+                    </div>
+
+                    <img
+                        src="~assets/images/header/arrow-left.svg"
+                        alt="arrow-left"
+                        class="ml-auto"
+                        @click="hideSideNav = true"
+                    />
                 </div>
 
-                <img
-                    src="~assets/images/header/arrow-left.svg"
-                    alt="arrow-left"
-                    class="ml-auto"
-                    @click="hideSideNav = true"
-                />
+                <UAccordion
+                    :items="navItems"
+                    color="black"
+                    size="xl"
+                    open-icon="i-heroicons-plus"
+                    close-icon="i-heroicons-minus"
+                    :ui="{
+                        item: { padding: 'p-0' },
+                    }"
+                    class="text-Primary-600-Dark-Primary px-7"
+                >
+                    <template #category="{ item }">
+                        <ul class="text-left bg-Primary-50 py-3 px-5 text-Neutral-900">
+                            <li
+                                v-for="(list, index) in item.lists"
+                                :key="index"
+                                class="mb-5 last:mb-0 cursor-pointer"
+                                @click="goToPage(list.link)"
+                            >
+                                <p>{{ list.name }}</p>
+                            </li>
+                        </ul>
+                    </template>
+
+                    <template #proposal="{ item }">
+                        <ul class="text-left bg-Primary-50 py-3 px-5 text-Neutral-900">
+                            <li
+                                v-for="(list, index) in item.lists"
+                                :key="index"
+                                class="mb-5 last:mb-0 cursor-pointer"
+                                @click="goToPage(list.link)"
+                            >
+                                <p>{{ list.name }}</p>
+                            </li>
+                        </ul>
+                    </template>
+
+                    <template #default="{ item, open }">
+                        <UButton
+                            color="white"
+                            variant="ghost"
+                            :ui="{ rounded: 'rounded-none' }"
+                            class="text-Primary-600-Dark-Primary disabled:opacity-100 text-base justify-between p-0 pt-4 pb-2"
+                            @click="goToPage(item.link)"
+                        >
+                            <span class="truncate">{{ item.label }}</span>
+
+                            <template #trailing>
+                                <div v-show="item.showOpenIcon">
+                                    <UIcon
+                                        v-if="open"
+                                        name="i-heroicons-minus"
+                                        class="w-5 h-5 ms-auto transform transition-transform duration-200 flex"
+                                    />
+                                    <UIcon
+                                        v-else
+                                        name=" i-heroicons-plus"
+                                        class="w-5 h-5 ms-auto transform transition-transform duration-200 flex"
+                                    />
+                                </div>
+                            </template>
+                        </UButton>
+                    </template>
+                </UAccordion>
             </div>
-
-            <UAccordion
-                :items="navItems"
-                color="black"
-                size="xl"
-                open-icon="i-heroicons-plus"
-                close-icon="i-heroicons-minus"
-                :ui="{
-                    item: { padding: 'p-0' },
-                }"
-                class="text-Primary-600-Dark-Primary px-7"
-            >
-                <template #category="{ item }">
-                    <ul class="text-left bg-Primary-50 py-3 px-5 text-Neutral-900">
-                        <li
-                            v-for="(list, index) in item.lists"
-                            :key="index"
-                            class="mb-5 last:mb-0 cursor-pointer"
-                            @click="goToPage(list.link)"
-                        >
-                            <p>{{ list.name }}</p>
-                        </li>
-                    </ul>
-                </template>
-
-                <template #proposal="{ item }">
-                    <ul class="text-left bg-Primary-50 py-3 px-5 text-Neutral-900">
-                        <li
-                            v-for="(list, index) in item.lists"
-                            :key="index"
-                            class="mb-5 last:mb-0 cursor-pointer"
-                            @click="goToPage(list.link)"
-                        >
-                            <p>{{ list.name }}</p>
-                        </li>
-                    </ul>
-                </template>
-
-                <template #default="{ item, open }">
-                    <UButton
-                        color="white"
-                        variant="ghost"
-                        :ui="{ rounded: 'rounded-none' }"
-                        class="text-Primary-600-Dark-Primary disabled:opacity-100 text-base justify-between p-0 pt-4 pb-2"
-                        @click="goToPage(item.link)"
-                    >
-                        <span class="truncate">{{ item.label }}</span>
-
-                        <template #trailing>
-                            <div v-show="item.showOpenIcon">
-                                <UIcon
-                                    v-if="open"
-                                    name="i-heroicons-minus"
-                                    class="w-5 h-5 ms-auto transform transition-transform duration-200 flex"
-                                />
-                                <UIcon
-                                    v-else
-                                    name=" i-heroicons-plus"
-                                    class="w-5 h-5 ms-auto transform transition-transform duration-200 flex"
-                                />
-                            </div>
-                        </template>
-                    </UButton>
-                </template>
-            </UAccordion>
         </div>
-    </div>
+    </header>
 </template>
 
 <script setup>
