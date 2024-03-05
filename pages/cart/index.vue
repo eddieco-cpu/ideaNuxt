a065496fd37ea852aa466b9ee0b9b81f4d364403
Ivan.chen <Ivan.chen@advantech.com.tw>
2024-03-05 17:38:40 +0800

    更新 | 購物車

--- pages/cart/index.vue
+++ pages/cart/index.vue
@@ -50,32 +50,36 @@
         </div>
 
         <!-- 總計 -->
-        <div class="flex-1 md:sticky md:top-[98px]">
-            <CardContainer title="總計">
-                <template #body>
-                    <div class="flex justify-between text-Neutral-700, text-sm">
-                        <p>2件商品</p>
-                        <p>NT$3,240</p>
-                    </div>
-                    <div class="flex justify-between text-Neutral-700, text-sm">
-                        <p>運費</p>
-                        <p>NT$240</p>
-                    </div>
-                </template>
+        <div class="flex-1 fixed w-full bottom-0 md:sticky md:top-[98px]">
+            <div class="max-md:h-screen max-md:flex max-md:items-end max-md:bg-black/50">
+                <CardContainer title="總計">
+                    <template #body>
+                        <div class="flex justify-between text-Neutral-700, text-sm">
+                            <p>2件商品</p>
+                            <p>NT$3,240</p>
+                        </div>
+                        <div class="flex justify-between text-Neutral-700, text-sm">
+                            <p>運費</p>
+                            <p>NT$240</p>
+                        </div>
+                    </template>
 
-                <template #footer>
-                    <div class="border-t border-t-Neutral-200">
-                        <p class="text-xl text-Primary-500-Primary font-roboto font-medium text-right py-3">NT$1,620</p>
+                    <template #footer>
+                        <div class="border-t border-t-Neutral-200">
+                            <p class="text-xl text-Primary-500-Primary font-roboto font-medium text-right py-3">
+                                NT$1,620
+                            </p>
 
-                        <button
-                            class="px-4 py-2 bg-Primary-500-Primary text-center rounded-lg w-full text-white"
-                            @click="goCheckoutPage"
-                        >
-                            去結帳 ({{ 3 }})
-                        </button>
-                    </div>
-                </template>
-            </CardContainer>
+                            <button
+                                class="px-4 py-2 bg-Primary-500-Primary text-center rounded-lg w-full text-white"
+                                @click="goCheckoutPage"
+                            >
+                                去結帳 ({{ 3 }})
+                            </button>
+                        </div>
+                    </template>
+                </CardContainer>
+            </div>
         </div>
     </div>
 </template>
