a9edd98dc9aa738df9d33928c9a8df7472fe7535
Ivan.chen <Ivan.chen@advantech.com.tw>
2024-03-06 09:46:48 +0800

    新增 | 好物分享頁

---
+++ pages/good-thing.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="flex flex-col items-center gap-y-4 mt-32">
+        <img src="~assets/images/status/emptyDataThing.svg" alt="empty" />
+        <p class="text-Primary-500-Primary font-medium text-xl">全新內容，敬請期待！</p>
+    </div>
+</template>
