f5bbc418148d461f69dfde4a734f241f60360402
udnlinyi <130973011+udnlinyi@users.noreply.github.com>
2024-03-07 16:49:42 +0800

    更新 | progress page; 更新 | dashboard/progress/*

---
+++ components/dashboard/DashboardProgressAccordion.vue
@@ -0,0 +1,32 @@
+<template>
+    <div @click="item.defaultOpen = !item.defaultOpen" class="overflow-hidden mb-6 cursor-pointer">
+        <h3 class="truncat text-lg font-semibold">{{ item.title }}</h3>
+        <div
+            class="grid grid-rows-[0fr] transition-all duration-200 ease-linear"
+            :class="item.defaultOpen ? 'grid-rows-[1fr]' : ''"
+        >
+            <section class="overflow-hidden">
+                <article class="py-4 text-base font-medium text-Neutral-700">
+                    <div v-html="item.html"></div>
+                </article>
+            </section>
+        </div>
+        <div class="pt-3 flex justify-between items-center">
+            <time class="text-xs text-Neutral-600-Dark-Primary">{{ item.date }}</time>
+            <span class="text-xs text-blue-800 underline">{{ item.defaultOpen ? "收起" : "展開" }}</span>
+        </div>
+    </div>
+</template>
+<script setup>
+const props = defineProps({
+    item: {
+        type: Object,
+        default: () => ({
+            title: "Getting Started",
+            defaultOpen: false,
+            html: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.</p>",
+            date: "2022/9/17",
+        }),
+    },
+});
+</script>
--- pages/dashboard/progress/[progressId].vue
+++ pages/dashboard/progress/[progressId].vue
@@ -1,7 +1,8 @@
 <template>
     <div>
         <UiTitle class="w-full">新增進度更新</UiTitle>
-        <section class="min-h-32 rounded-lg border-[1px] border-[#E5E5E5] bg-white mb-3">
+        <section class="min-h-32 rounded-lg border-[1px] border-[#E5E5E5] bg-white mb-3 p-6">
+            <!--  -->
             <UFormGroup label="標題" required class="mb-9">
                 <div class="relative">
                     <UTextarea
@@ -18,13 +19,28 @@
                 </div>
             </UFormGroup>
 
+            <!--  -->
             <UFormGroup label="更新內容" required class="mb-6">
-                <DashboardProgressFullContext />
+                <DashboardProgressFullContext ref="fullContext" />
             </UFormGroup>
+
+            <div class="flex justify-start items-center gap-x-2">
+                <UiButton class="max-w-16 max-h-8 !text-sm" @click="doSubmit">發佈</UiButton>
+                <UiButton class="max-w-16 max-h-8 !text-sm" type="secondary">刪除</UiButton>
+            </div>
         </section>
     </div>
 </template>
 <script setup>
+const fullContext = ref();
+
 const title = ref("");
 const titleMaxLength = 40;
+
+function doSubmit() {
+    console.log("doSubmit");
+    console.log("title", title.value);
+    console.log("fullContext", fullContext.value.editorContent);
+    fullContext.value.quillEditorBody.setHTML("");
+}
 </script>
